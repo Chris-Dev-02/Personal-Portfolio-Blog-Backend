b80f4c2f380d82980a9b0be752e52fab4561b7d7
Chris <cmartinez.bitworks.com.sv>
2025-11-02 16:50:04 -0600

    feat(entry-content-models): Define Technology model

--- apps/entry_content/models.py
+++ apps/entry_content/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
+import uuid
 
 # Create your models here.
+class Technology(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    user_owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name='technologies')
+    name = models.CharField(max_length=150)
+    thumbnail = models.FileField(upload_to='content_files/', blank=True, null=True)
+    description = models.TextField()
+    created_at = models.DateTimeField(null=True, blank=True)
+
+    class Meta:
+        verbose_name_plural = "Technologies"
+
+        ordering = ['name']
+
+    def __str__(self) -> str:
+        return f"{self.name} - by {self.user_owner.username}"
