fc4aab10a2e9583d35a924b2c3c2d5fe1bb7d84e
Chris <cmartinez.bitworks.com.sv>
2025-11-03 17:19:00 -0600

    feat(entry-content-serializers): Define ContentBlock serializer

--- apps/entry_content/serializers.py
+++ apps/entry_content/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Technology
+from .models import Technology, ContentBlock
 
 class TechnologySerializer(serializers.ModelSerializer):
     user_owner = serializers.StringRelatedField()  # Read-only, displays the username
@@ -14,3 +14,35 @@ class TechnologySerializer(serializers.ModelSerializer):
             'created_at',
             'user_owner'
         ]
+
+class ContentBlockSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ContentBlock
+        fields = [
+            'id',
+            'type',
+            'text_content',
+            'file',
+            'order',
+            'created_at',
+            'updated_at',
+            'article',
+        ]
+        read_only_fields = ['created_at', 'updated_at']
+
+    def validate(self, data):
+        type = data.get('type')
+        text_content = data.get('text_content')
+        file = data.get('file')
+
+        if type == ContentBlock.TEXT:
+            if not text_content:
+                raise serializers.ValidationError('Text content is required for text blocks.')
+            if file:
+                raise serializers.ValidationError('Text blocks must not have a file.')
+        else:
+            if not file:
+                raise serializers.ValidationError('Multimedia blocks must have a file.')
+            if text_content:
+                raise serializers.ValidationError('Multimedia blocks must not have text content.')
+        return data
