507bd1b8dc61fa92d97a68ca9cc444ca4de8c041
Chris <cmartinez.bitworks.com.sv>
2026-02-07 14:45:09 -0600

    feat(entry-content): Add mvt_urls in app urls

--- apps/entry_content/urls.py
+++ apps/entry_content/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path, include
 
+app_name = "entry_content"
+
 urlpatterns = [
-    path('v1/', include('apps.entry_content.urls_v1')),
+    path('v1/', include('apps.entry_content.api_urls_v1')),
+    path('', include('apps.entry_content.mvt_urls')),
 ]
