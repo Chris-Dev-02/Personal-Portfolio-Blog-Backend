babdbb0ac4a51409c42be0feddd53a35c7e0b598
Chris <cmartinez.bitworks.com.sv>
2026-02-07 14:41:24 -0600

    feat(entry-content): Add slug to technology model

--- apps/entry_content/models.py
+++ apps/entry_content/models.py
@@ -12,6 +12,7 @@ class Technology(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user_owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='technologies')
     name = models.CharField(max_length=150)
+    slug = models.SlugField(blank=True)
     thumbnail = models.FileField(upload_to='content_files/technology', blank=True, null=True)
     description = models.TextField()
     created_at = models.DateTimeField(null=True, blank=True)
@@ -21,6 +22,11 @@ class Technology(models.Model):
         verbose_name_plural = "Technologies"
 
         ordering = ['name']
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super().save(*args, **kwargs)
 
     def __str__(self) -> str:
         return f"{self.name} - by {self.user_owner.username}"
